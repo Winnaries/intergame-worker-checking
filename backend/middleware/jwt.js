5fd50a111062c372b8bad39646dec3e7d1c496e2
winnaries <w.winnaries@gmail.com>
2020-02-10 17:38:52 +0700

    Secure routes

--- backend/middleware/jwt.js
+++ backend/middleware/jwt.js
@@ -1,16 +1,19 @@
 const passport = require('passport');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
-const auth = (new require('../secure/auth'))();
+const auth = new (require('../secure/auth'))();
 
-const options = {};
-options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-options.secretOrKey = "DudeItsSecret";
-passport.use(new JwtStrategy(options, (payload, done) => {
-    if (!payload.passphrase) return done(null, false);
-    if (auth.validatePayload(payload.passphrase)) {
-        return done(null, { code: "CommitteeAY2019" });
-    } else {
-        return done(null, false);
-    }
-}));
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = "DudeItsSecret";
+
+module.exports = passport => {
+    passport.use(new JwtStrategy(opts, (payload, done) => {
+        if (!payload.passphrase) return done(null, false);
+        if (auth.validatePayload(payload.passphrase)) {
+            return done(null, {code: "CommitteeAY2019"});
+        } else {
+            return done(null, false);
+        }
+    }));
+}
--- backend/secure/auth.js
+++ backend/secure/auth.js
@@ -5,28 +5,28 @@ const config = require('config');
 class AuthController {
 
     async login(req, res) {
-        const result = await this.validate(req.body.password);
+        const result = await validate(req.body.password);
         if (result) res.json({ token: await result });
-        else res.send(401);
-    }
-
-    async validate(password) {
-        if (await bcrypt.compare(password, config.get('hash')))
-            return this.getPayload();
-        return null;
+        else res.sendStatus(401);
     }
 
     validatePayload(passphrase) {
         return passphrase === "CommitteeAY2019";
     }
 
-    async getPayload() {
-        return jwt.sign({
-            exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24),
-            passphrase: "CommitteeAY2019"
-        }, 'DudeItsSecret');
-    }
+}
 
+async function validate(password) {
+    if (await bcrypt.compare(password, config.get('hash')))
+        return getPayload();
+    return null;
+}
+
+async function getPayload() {
+    return jwt.sign({
+        exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24),
+        passphrase: "CommitteeAY2019"
+    }, 'DudeItsSecret');
 }
 
 module.exports = AuthController;
